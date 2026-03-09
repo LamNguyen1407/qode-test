65c5cfbd5e7fbe5594b3fa54412e1b24dd9b8c95
LamNguyen1407 <ln14072004@gmail.com>
2026-03-09 18:44:06 +0700

    fix name of env

--- lib/s3.ts
+++ lib/s3.ts
@@ -1,8 +1,8 @@
 import AWS from "aws-sdk";
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY!,
-  secretAccessKey: process.env.AWS_SECRET_KEY!,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
   region: process.env.AWS_REGION!,
 });
 
